212e4e4c4b287a03f42e7850a0356cb80ebf85fb
tzx <tangzhixin@tangzhixinMacBook-Pro.local>
2018-05-18 17:19:07 +0800

    create project

---
+++ videoframe/videoframe.h
@@ -0,0 +1,25 @@
+//
+//  videoframe.h
+//  videoframe
+//
+//  Created by tangzhixin on 2018/5/17.
+//  Copyright © 2018年 tangzhixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for videoframe.
+FOUNDATION_EXPORT double videoframeVersionNumber;
+
+//! Project version string for videoframe.
+FOUNDATION_EXPORT const unsigned char videoframeVersionString[];
+
+
+#import <videoframe/RTCVideoFrameBuffer.h>
+#import <videoframe/video_frame.h>
+#import <videoframe/RTCVideoFrame+Private.h>
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <videoframe/PublicHeader.h>
+
+
